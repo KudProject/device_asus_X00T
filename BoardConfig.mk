5c4d3252e1d308ff8ee2619883900ab0086e3d37
Cosme Domínguez Díaz <cosme.ddiaz@gmail.com>
2018-04-16 21:14:16 +0200

    X00T: Drop TARGET_USES_NEW_ION_API flag
      * Deprecated
    
    Change-Id: Id50d6721cb1fb96bdb6d3ddb40b36a0147094b86

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -80,7 +80,6 @@ TARGET_USES_HWC2 := true
 TARGET_USES_GRALLOC1 := true
 TARGET_USES_ION := true
 TARGET_SCREEN_DENSITY := 420
-TARGET_USES_NEW_ION_API :=true
 
 # DT2W
 TARGET_TAP_TO_WAKE_NODE := "/sys/kernel/touchpanel/dclicknode"
