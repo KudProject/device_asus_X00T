3cb67bb460a92c5a2b2e9081fd91535848e0a5b2
ShivamKumarJha <jha.shivam3@gmail.com>
2018-05-07 17:36:08 +0530

    X00T: HIDL: build android.hidl.manager@1.0-java

--- hidl-hals.mk
+++ hidl-hals.mk
@@ -76,7 +76,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     android.hidl.base@1.0_system \
-    android.hidl.manager@1.0
+    android.hidl.manager@1.0 \
+    android.hidl.manager@1.0-java
     
 # Netutils
 PRODUCT_PACKAGES += \
