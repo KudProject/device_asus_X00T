16d7eee45a4a1a91a707fce211f2eabf6d248294
Albert I <krascgq@outlook.co.id>
2018-09-23 16:52:13 +0800

    X00T: Set BOARD_SEPOLICY_VERS
    Silences the following warning:
      system/sepolicy/Android.mk:79: warning: BOARD_SEPOLICY_VERS not specified, assuming current platform version
    
    Change-Id: I33f7167c8deb3fce1226c17bb86ac7cefc70e700
    Signed-off-by: Albert I <krascgq@outlook.co.id>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -253,6 +253,7 @@ VENDOR_SECURITY_PATCH := 2018-08-01
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
+BOARD_SEPOLICY_VERS := 28.0
 #BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
 
 # Use Snapdragon LLVM, if available
