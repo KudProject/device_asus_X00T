6b78ef186a727490d1338688486648180aa3add0
Anton Hansson <hansson@google.com>
2018-11-07 16:09:35 +0000

    X00T: Remove obsolete dex settings
    This block is completely redundant because..
    - WITH_DEXPREOPT := true is the default
    - WITH_DEXPREOPT_PIC is never read
    - DEX_PREOPT_DEFAULT := nostripping has no effect when the
      BOARD_USES_SYSTEM_OTHER_ODEX flag is set to true
    
    Test: make
    Change-Id: I618a1e395ca1a14555cd50eef1a4f1aa079b553d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -88,18 +88,6 @@ TARGET_TAP_TO_WAKE_NODE := "/sys/kernel/touchpanel/dclicknode"
 # DRM
 TARGET_ENABLE_MEDIADRM_64 := true
 
-# Enable dex pre-opt to speed up initial boot
-ifeq ($(HOST_OS),linux)
-    ifeq ($(WITH_DEXPREOPT),)
-      WITH_DEXPREOPT := true
-      WITH_DEXPREOPT_PIC := false
-      ifneq ($(TARGET_BUILD_VARIANT),user)
-        # Retain classes.dex in APK's for non-user builds
-        DEX_PREOPT_DEFAULT := nostripping
-      endif
-    endif
-endif
-
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
 
