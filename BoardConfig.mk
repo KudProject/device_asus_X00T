291589b9b685aec4ebab430f7aa991999dd65ecb
Dede Dindin Qudsy <xtrymind@gmail.com>
2019-02-09 10:41:37 +0700

    X00T: include device sepolicy
    Signed-off-by: Dede Dindin Qudsy <xtrymind@gmail.com>
    Signed-off-by: Albert I <krascgq@outlook.co.id>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -240,7 +240,7 @@ VENDOR_SECURITY_PATCH := 2019-02-01
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
-#BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
+BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
 
 # Use Snapdragon LLVM, if available
 TARGET_USE_SDCLANG := true
