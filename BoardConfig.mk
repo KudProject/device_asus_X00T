e9de080b50cdb0ca44ac35a1edd7e2e59b19518c
Albert I <kras@raphielgang.org>
2019-05-13 03:00:01 +0800

    Revert "X00T: Bye bye ASUS_X00TD assert support"
    This reverts commit b0040430740e9330c9bbba35a70d7a0e198a9ff1.
    
    Add it back as my latest TWRP has ro.product.device set to ASUS_X00TD
    so it can flash stock firmware without having updater-script modified.
    
    Signed-off-by: Albert I <kras@raphielgang.org>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -51,7 +51,7 @@ TARGET_USES_64_BIT_BINDER := true
 
 # Assert
 TARGET_BOARD_INFO_FILE := $(DEVICE_PATH)/board-info.txt
-TARGET_OTA_ASSERT_DEVICE := X00TD,X00T
+TARGET_OTA_ASSERT_DEVICE := ASUS_X00TD,X00TD,X00T
 
 # Kernel
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200,n8 androidboot.console=ttyMSM0 earlycon=msm_serial_dm,0xc170000 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 sched_enable_hmp=1 sched_enable_power_aware=1 service_locator.enable=1 swiotlb=1 firmware_class.path=/vendor/firmware_mnt/image loop.max_part=7
