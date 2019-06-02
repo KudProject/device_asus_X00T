f535040f94a99d554af73b23ea19755ac2f67e47
Luca Stefani <luca020400@lineageos.org>
2019-06-02 12:20:28 +0000

    X00T: Make Android.mk search recursive Change-Id: I3a05344f949eae581f1f17cc33b1df475d3e6760

--- Android.mk
+++ Android.mk
@@ -18,7 +18,8 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),X00T)
 
-include $(call all-makefiles-under,$(LOCAL_PATH))
+subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
+$(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
 include $(CLEAR_VARS)
 
 # A/B builds require us to create the mount points at compile time.
