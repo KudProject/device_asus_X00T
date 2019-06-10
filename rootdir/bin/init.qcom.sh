3157e968ee596742b289b7ac8736e0a81a14eda4
kazerine <kamikaze.mrinal@gmail.com>
2019-06-10 13:46:01 +0530

    X00T: init: clear settings cache
    * fixes Settings app crashing on dirty flashing
    
    Change-Id: Ia5603dfc4da1bd34408b4e57e12b088de3ded603
    Signed-off-by: Mrinal Kant Shukla <kamikaze.mrinal@gmail.com>

--- rootdir/bin/init.qcom.sh
+++ rootdir/bin/init.qcom.sh
@@ -463,3 +463,6 @@ case "$buildvariant" in
         echo "4 4 1 4" > /proc/sys/kernel/printk
         ;;
 esac
+
+# Remove settings cache, avoids derps after dirty flash
+rm -rf /data/system/package_cache
