af49e0226d480bf68228eeebe9e8336700077132
anomalchik <vidslava@yandex.ru>
2019-08-23 19:50:10 +0000

    X00T: add dummy keylayouts for fingerprints This is make to prevent the use key codes from Generic.kl Fingerprint gestures not yet implemented in this ROM so this files is empty

--- device.mk
+++ device.mk
@@ -51,4 +51,6 @@ PRODUCT_COPY_FILES += \
 
 # Keylayout
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gpio-keys.kl
+    $(LOCAL_PATH)/configs/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gpio-keys.kl \
+    $(LOCAL_PATH)/configs/keylayout/gxfp_input.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gxfp_input.kl \
+    $(LOCAL_PATH)/configs/keylayout/cdfinger_input.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/cdfinger_input.kl
