9eebbd1deb7dc2dc27eb4399ca061e8f2cf3f3b3
Pranav Kapoor <pranavkapoor001@gmail.com>
2020-05-04 17:29:36 +0530

    X00T: Initial device specific tree
    Signed-off-by: Pranav Kapoor <pranavkapoor001@gmail.com>

---
+++ lineage_X00T.mk
@@ -0,0 +1,39 @@
+#
+# Copyright (C) 2020 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Lineage stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit from X00T device
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+PRODUCT_BRAND := asus
+PRODUCT_DEVICE := X00T
+PRODUCT_MANUFACTURER := asus
+PRODUCT_NAME := lineage_X00T
+PRODUCT_MODEL := ASUS_X00T
+
+PRODUCT_GMS_CLIENTID_BASE := android-asus
+TARGET_VENDOR := asus
+TARGET_VENDOR_PRODUCT_NAME := X00T
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="sdm660_64-user 10 QKQ1 32 release-keys"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := sdm660_64-user-10-QKQ1-32-release-keys
