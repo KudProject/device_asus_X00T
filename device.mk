e0c55016ff1a8ef9c1990d44becf547495086a98
Pranav Kapoor <pranavkapoor001@gmail.com>
2020-05-05 17:19:17 +0530

    X00T: Import sensors hals.conf
    Signed-off-by: Pranav Kapoor <pranavkapoor001@gmail.com>

--- device.mk
+++ device.mk
@@ -230,6 +230,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
     $(LOCAL_PATH)/seccomp/mediaextractor-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
     
+# Sensors
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
+    
 # VNDK
 # Update this list with blob function
 # libstdc++: camera.sdm660
