f921488ca75a7899256bc82eb2a8eb3a13402d9d
Pranav Kapoor <pranavkapoor001@gmail.com>
2020-05-08 00:31:10 +0530

    X00T: Build DRM packages
    Signed-off-by: Pranav Kapoor <pranavkapoor001@gmail.com>

--- hidl-hals.mk
+++ hidl-hals.mk
@@ -54,6 +54,12 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@2.0-impl-2.1 \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service
+    
+# DRM
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.0-impl \
+    android.hardware.drm@1.0-service \
+    android.hardware.drm@1.2-service.clearkey
 
 # FM
 PRODUCT_PACKAGES += \
