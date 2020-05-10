23c05697bab99e1dd867c63a338a4502d3f6a6e5
Pranav Kapoor <pranavkapoor001@gmail.com>
2020-05-10 21:44:20 +0530

    [TEMP] X00T: Copy some audio libs to vendor
    Signed-off-by: Pranav Kapoor <pranavkapoor001@gmail.com>

--- vndk/vndk-ext-libs.mk
+++ vndk/vndk-ext-libs.mk
@@ -1,2 +1,6 @@
 EXTRA_VENDOR_LIBRARIES := \
-    android.hidl.base@1.0
+    android.hidl.base@1.0 \
+    android.hardware.audio.common@5.0 \
+    android.hardware.audio.effect@5.0 \
+    android.hardware.audio@5.0 \
+    libfmq
