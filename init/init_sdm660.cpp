64587bfa361863fdf29d842477763de97b6e3d8b
Rk779 <rahulkrishna585@gmail.com>
2020-06-06 17:17:23 +0530

    X00T: libinit: Set prop based on nfc hw availability
    Change-Id: I599ccebc25a2e4c136c30ada65703962a9e75aad

--- init/init_sdm660.cpp
+++ init/init_sdm660.cpp
@@ -88,9 +88,23 @@ void check_device()
     }
 }
 
+void NFC_check()
+{
+    // Check NFC
+    std::ifstream infile("/proc/NFC_CHECK");
+    std::string check;
+
+    getline(infile, check);
+    if (!check.compare("SUPPORTED"))
+        property_override("ro.hq.support.nfc", "1");
+    else
+        property_override("ro.hq.support.nfc", "0");
+}
+
 void vendor_load_properties()
 {
     check_device();
+    NFC_check();
 
     property_override("dalvik.vm.heapstartsize", heapstartsize);
     property_override("dalvik.vm.heapgrowthlimit", heapgrowthlimit);
