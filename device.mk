b825beba3b7a3f84ab359aa994ce752994323d57
Dan Pasanen <invisiblek@cyanogenmod.org>
2020-06-10 15:36:47 +0530

    X00T: Exclude lineage-sdk overlays from RRO
    * lineage-sdk overlays don't work with RRO
    
    Change-Id: Iff826faf1864388066a488acc9682636b98d1098

--- device.mk
+++ device.mk
@@ -243,6 +243,8 @@ DEVICE_PACKAGE_OVERLAYS += \
     
 # Overlays - Enforce RRO
 PRODUCT_ENFORCE_RRO_TARGETS := *
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-lineage/lineage-sdk
 
 # Permissions
 PRODUCT_COPY_FILES += \
