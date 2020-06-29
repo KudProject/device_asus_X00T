786d4f5377de9b954b90a35f794e29c39af599cb
Pranav Kapoor <pranavkapoor001@gmail.com>
2020-06-29 17:23:53 +0530

    X00T: rootdir: Cleanup
    Signed-off-by: Pranav Kapoor <pranavkapoor001@gmail.com>

--- device.mk
+++ device.mk
@@ -297,34 +297,20 @@ PRODUCT_PACKAGES += \
     
 # Ramdisk Scripts
 PRODUCT_PACKAGES += \
-    emmc_prop_init.sh \
     init.class_main.sh \
-    init.crda.sh \
     init.hw.qcom.sh \
-    init.mdm.sh \
-    init.qcom.class_core.sh \
-    init.qcom.coex.sh \
-    init.qcom.crashdata.sh \
-    init.qcom.early_boot.sh \
-    init.qcom.efs.sync.sh \
     init.qcom.post_boot.sh \
-    init.qcom.sdio.sh \
     init.qcom.sensors.sh \
     init.qcom.sh \
     init.qcom.usb.sh \
-    init.qti.can.sh \
-    init.qti.fm.sh \
-    init.qti.ims.sh \
-    init.qti.qseecomd.sh \
-    qca6234-service.sh
-    
+    init.qti.qseecomd.sh
+
 # Ramdisk rc
 PRODUCT_PACKAGES += \
     fstab.qcom \
     init.amt.rc \
     init.amt.target.rc \
     init.msm.usb.configfs.rc \
-    init.qcom.factory.rc \
     init.qcom.rc \
     init.qcom.usb.rc \
     init.target.rc \
