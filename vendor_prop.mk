03fc0add29c7efd947f8889fca13e4c4ea71739e
Pranav Kapoor <pranavkapoor001@gmail.com>
2020-07-21 11:12:38 +0530

    X00T: Reorganize vendor props
    * Also drop stale properties (unused or set by default):
       ro.control_privapp_permissions=enforce
       ro.hardware.egl=adreno
       ro.hardware.vulkan=adreno
       ro.product.first_api_level=27
       ro.vendor.manufacturer.product.name=zb601kl_m1
       vendor.power.pasr.enabled=true
    
    Signed-off-by: Pranav Kapoor <pranavkapoor001@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -1,76 +1,42 @@
-PRODUCT_PROPERTY_OVERRIDES +=  \
+# Audio
+PRODUCT_PROPERTY_OVERRIDES += \
     af.fast_track_multiplier=1 \
     audio.deep_buffer.media=true \
     audio.offload.min.duration.secs=30 \
     audio.offload.video=true \
     audio.sys.noisy.broadcast.delay=600 \
     audio.sys.offload.pstimeout.secs=3 \
-    dalvik.vm.heapgrowthlimit=256m \
-    dalvik.vm.heapmaxfree=8m \
-    dalvik.vm.heapminfree=512k \
-    dalvik.vm.heapsize=512m \
-    dalvik.vm.heapstartsize=8m \
-    dalvik.vm.heaptargetutilization=0.75 \
-    debug.stagefright.omx_default_rank.sw-audio=1 \
-    debug.stagefright.omx_default_rank=0 \
-    keyguard.no_require_sim=true \
-    persist.backup.ntpServer=0.pool.ntp.org \
-    persist.bluetooth.a2dp_offload.cap=sbc-aac-aptx-aptxhd-ldac \
-    persist.bluetooth.a2dp_offload.disabled=true \
-    persist.vendor.service.bdroid.sibs=false \
-    persist.radio.multisim.config=dsds \
     persist.vendor.audio.fluence.speaker=false \
     persist.vendor.audio.fluence.voicecall=true \
     persist.vendor.audio.fluence.voicerec=true \
     persist.vendor.audio.hifi.int_codec=true \
     persist.vendor.audio.hw.binder.size_kbyte=1024 \
     persist.vendor.audio.ras.enabled=false \
-    persist.vendor.bt.a2dp_offload_cap=sbc-aptx-aptxhd-aac \
-    persist.vendor.bt.aac_frm_ctl.enabled=true \
-    persist.vendor.camera.expose.aux=1 \
-    persist.vendor.camera.mpo.disabled=1 \
-    persist.vendor.qcom.bluetooth.a2dp_offload_cap=sbc-aptx-aptxhd-aac-ldac \
-    persist.vendor.qcom.bluetooth.enable.splita2dp=true \
-    persist.vendor.qcomsysd.enabled=1 \
-    persist.vendor.radio.apm_sim_not_pwdn=1 \
-    persist.vendor.radio.custom_ecc=1 \
-    persist.vendor.radio.procedure_bytes=SKIP \
-    persist.vendor.radio.rat_on=combine \
-    persist.vendor.radio.sib16_support=1 \
-    persist.dbg.volte_avail_ovr=1 \
-    persist.dbg.vt_avail_ovr=1 \
-    persist.dbg.wfc_avail_ovr=1 \
-    qemu.hw.mainkeys=0 \
-    rild.libpath=/system/vendor/lib64/libril-qc-qmi-1.so \
-    ro.additionalbutton.operation=0 \
     ro.af.client_heap_size_kbyte=7168 \
-    ro.bluetooth.a2dp_offload.supported=false \
-    ro.carrier=unknown \
-    ro.com.android.dataroaming=false \
-    ro.com.google.rlz_ap_whitelist=y0,y5,y6,y7,y9 \
-    ro.com.google.rlzbrandcode=ASUP \
-    ro.control_privapp_permissions=enforce \
-    ro.frp.pst=/dev/block/bootdevice/by-name/frp \
-    ro.hardware.egl=adreno \
-    ro.hardware.nfc_nci=nqx.default \
-    ro.hardware.vulkan=adreno \
-    ro.opengles.version=196610 \
-    ro.product.first_api_level=27 \
-    ro.ril.ecclist=112,911 \
-    ro.telephony.iwlan_operation_mode=legacy \
-    ro.telephony.use_old_mnc_mcc_format=true \
     ro.vendor.audio.sdk.fluencetype=fluence \
     ro.vendor.audio.sdk.ssr=false \
-    ro.vendor.bluetooth.wipower=false \
-    ro.vendor.extension_library=libqti-perfd-client.so \
-    ro.vendor.icepower.effect.enable=true \
-    ro.vendor.manufacturer.product.name=zb601kl_m1 \
-    ro.vendor.qti.sdk.sensors.gestures=false \
-    ro.vendor.qti.sensors.cmc=false \
-    ro.vendor.qti.sensors.facing=false \
-    sys.vendor.shutdown.waittime=500 \
     vendor.audio.dolby.ds2.enabled=false \
     vendor.audio.dolby.ds2.hardbypass=false \
+    vendor.audio.flac.sw.decoder.24bit=true \
+    vendor.audio.hw.aac.encoder=true \
+    vendor.audio.offload.buffer.size.kb=64 \
+    vendor.audio.offload.gapless.enabled=true \
+    vendor.audio.offload.multiaac.enable=true \
+    vendor.audio.offload.multiple.enabled=false \
+    vendor.audio.offload.passthrough=false \
+    vendor.audio.offload.track.enable=true \
+    vendor.audio.parser.ip.buffer.size=262144 \
+    vendor.audio.safx.pbe.enabled=false \
+    vendor.audio.spkr_prot.tx.sampling_rate=48000 \
+    vendor.audio.tunnel.encode=false \
+    vendor.audio.use.sw.alac.decoder=true \
+    vendor.audio.use.sw.ape.decoder=true \
+    vendor.audio.volume.headset.gain.depcal=true \
+    vendor.audio_hal.period_size=192 \
+    vendor.voice.path.for.pcm.voip=false
+
+# Audio feature
+PRODUCT_PROPERTY_OVERRIDES += \
     vendor.audio.feature.a2dp_offload.enable=false \
     vendor.audio.feature.afe_proxy.enable=true \
     vendor.audio.feature.anc_headset.enable=true \
@@ -113,27 +79,101 @@ PRODUCT_PROPERTY_OVERRIDES +=  \
     vendor.audio.feature.usb_offload_sidetone_volume.enable=false \
     vendor.audio.feature.vbat.enable=true \
     vendor.audio.feature.wsa.enable=false \
-    vendor.audio.flac.sw.decoder.24bit=true \
-    vendor.audio.hw.aac.encoder=true \
-    vendor.audio.offload.buffer.size.kb=64 \
-    vendor.audio.offload.gapless.enabled=true \
-    vendor.audio.offload.multiaac.enable=true \
-    vendor.audio.offload.multiple.enabled=false \
-    vendor.audio.offload.passthrough=false \
-    vendor.audio.offload.track.enable=true \
-    vendor.audio.parser.ip.buffer.size=262144 \
-    vendor.audio.safx.pbe.enabled=false \
-    vendor.audio.spkr_prot.tx.sampling_rate=48000 \
-    vendor.audio.tunnel.encode=false \
-    vendor.audio.use.sw.alac.decoder=true \
-    vendor.audio.use.sw.ape.decoder=true \
-    vendor.audio.volume.headset.gain.depcal=true \
-    vendor.audio_hal.period_size=192 \
-    vendor.camera.aux.packagelist=org.codeaurora.snapcam \
-    vendor.hw.fm.init=0 \
-    vendor.power.pasr.enabled=true \
-    vendor.qcom.bluetooth.soc=cherokee \
-    vendor.vidc.dec.enable.downscalar=0 \
-    vendor.vidc.enc.disable.pq=true \
+    ro.vendor.icepower.effect.enable=true
+
+# Bluetooth
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.bluetooth.a2dp_offload.cap=sbc-aac-aptx-aptxhd-ldac \
+    persist.bluetooth.a2dp_offload.disabled=true \
+    persist.vendor.bt.a2dp_offload_cap=sbc-aptx-aptxhd-aac \
+    persist.vendor.bt.aac_frm_ctl.enabled=true \
+    persist.vendor.qcom.bluetooth.a2dp_offload_cap=sbc-aptx-aptxhd-aac-ldac \
+    persist.vendor.qcom.bluetooth.enable.splita2dp=true \
+    persist.vendor.service.bdroid.sibs=false \
+    ro.bluetooth.a2dp_offload.supported=false \
+    ro.vendor.bluetooth.wipower=false \
+    vendor.qcom.bluetooth.soc=cherokee
+
+# Camera
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.camera.expose.aux=1 \
+    persist.vendor.camera.mpo.disabled=1 \
+    vendor.camera.aux.packagelist=org.codeaurora.snapcam
+
+# Dalvik overrides
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.heapgrowthlimit=256m \
+    dalvik.vm.heapmaxfree=8m \
+    dalvik.vm.heapminfree=512k \
+    dalvik.vm.heapsize=512m \
+    dalvik.vm.heapstartsize=8m \
+    dalvik.vm.heaptargetutilization=0.75
+
+# Display
+PRODUCT_PROPERTY_OVERRIDES += \
     vendor.video.disable.ubwc=1 \
-    vendor.voice.path.for.pcm.voip=false
+    ro.opengles.version=196610
+
+# FM
+PRODUCT_PROPERTY_OVERRIDES += \
+    vendor.hw.fm.init=0
+
+# FRP
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.frp.pst=/dev/block/bootdevice/by-name/frp
+
+# GMS
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.com.google.rlz_ap_whitelist=y0,y5,y6,y7,y9 \
+    ro.com.google.rlzbrandcode=ASUP
+
+# Media
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.stagefright.omx_default_rank.sw-audio=1 \
+    debug.stagefright.omx_default_rank=0 \
+    vendor.vidc.enc.disable.pq=true \
+    vendor.vidc.dec.enable.downscalar=0
+
+# Misc
+PRODUCT_PROPERTY_OVERRIDES += \
+    keyguard.no_require_sim=true \
+    persist.vendor.qcomsysd.enabled=1 \
+    qemu.hw.mainkeys=0 \
+    ro.additionalbutton.operation=0
+
+# NFC
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hardware.nfc_nci=nqx.default
+
+# QTI
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.extension_library=libqti-perfd-client.so
+
+# Radio
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.backup.ntpServer=0.pool.ntp.org \
+    persist.dbg.volte_avail_ovr=1 \
+    persist.dbg.vt_avail_ovr=1 \
+    persist.dbg.wfc_avail_ovr=1 \
+    persist.radio.multisim.config=dsds \
+    persist.vendor.radio.apm_sim_not_pwdn=1 \
+    persist.vendor.radio.custom_ecc=1 \
+    persist.vendor.radio.procedure_bytes=SKIP \
+    persist.vendor.radio.rat_on=combine \
+    persist.vendor.radio.sib16_support=1 \
+    rild.libpath=/system/vendor/lib64/libril-qc-qmi-1.so \
+    ro.carrier=unknown \
+    ro.com.android.dataroaming=false \
+    ro.ril.ecclist=112,911 \
+    ro.telephony.iwlan_operation_mode=legacy \
+    ro.telephony.use_old_mnc_mcc_format=true
+
+# Sensor
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.qti.sdk.sensors.gestures=false \
+    ro.vendor.qti.sensors.cmc=false \
+    ro.vendor.qti.sensors.facing=false
+
+# Shutdown waitime
+PRODUCT_PROPERTY_OVERRIDES += \
+    sys.vendor.shutdown.waittime=500
