649a9a7a555059bf39c9a5d264ed694ec959bd5c
Pranav Kapoor <pranavkapoor001@gmail.com>
2020-08-07 21:41:16 +0530

    X00T: Update Build fp for august sp
    Signed-off-by: Pranav Kapoor <pranavkapoor001@gmail.com>

--- lineage_X00T.mk
+++ lineage_X00T.mk
@@ -36,10 +36,10 @@ PRODUCT_MODEL := ASUS_X00T
 PRODUCT_GMS_CLIENTID_BASE := android-asus
 TARGET_VENDOR := asus
 TARGET_VENDOR_PRODUCT_NAME := X00T
-PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="coral-user 10 QQ3A.200705.002 6506677 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="coral-user 10 QQ3A.200805.001 6578210 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := google/coral/coral:10/QQ3A.200705.002/6506677:user/release-keys
+BUILD_FINGERPRINT := google/coral/coral:10/QQ3A.200805.001/6578210:user/release-keys
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.build.fingerprint=$(BUILD_FINGERPRINT)
