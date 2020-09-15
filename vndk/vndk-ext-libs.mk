ff1b567e4cd0775fa0af7c90ecef8582a023e843
Pranav Kapoor <pranavkapoor001@gmail.com>
2020-09-15 18:56:06 +0530

    Revert "[TEMP] X00T: Copy some audio libs to vendor"
    This reverts commit 23c05697bab99e1dd867c63a338a4502d3f6a6e5.

--- vndk/vndk-ext-libs.mk
+++ vndk/vndk-ext-libs.mk
@@ -1,6 +1,2 @@
 EXTRA_VENDOR_LIBRARIES := \
-    android.hidl.base@1.0 \
-    android.hardware.audio.common@5.0 \
-    android.hardware.audio.effect@5.0 \
-    android.hardware.audio@5.0 \
-    libfmq
+    android.hidl.base@1.0
