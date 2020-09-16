7aa507082b392b6ebbe28d8d07fb8b26b06571ad
Pranav Kapoor <pranavkapoor001@gmail.com>
2020-09-16 17:43:23 +0530

    X00T: Add property to enable Blur on Android 11
    Signed-off-by: Pranav Kapoor <pranavkapoor001@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -191,5 +191,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # SurfaceFlinger
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.max_virtual_display_dimension=4096 \
+    ro.surface_flinger.supports_background_blur=1 \
     ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
     ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000
