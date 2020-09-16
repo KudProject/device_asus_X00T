15efd1f2c6c94ff27efd270a257988f4334f06f1
Pranav Kapoor <pranavkapoor001@gmail.com>
2020-09-16 16:35:17 +0530

    X00T: Uprev SoundTrigger to 2.2
    Signed-off-by: Pranav Kapoor <pranavkapoor001@gmail.com>

--- hidl-hals.mk
+++ hidl-hals.mk
@@ -20,7 +20,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@5.0-impl \
     android.hardware.audio.effect@2.0-service \
-    android.hardware.soundtrigger@2.1-impl
+    android.hardware.soundtrigger@2.2-impl
     
 # Bluetooth
 PRODUCT_PACKAGES += \
