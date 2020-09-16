ec4723aede0f44422cd42d46f9839744b5822453
Pranav Kapoor <pranavkapoor001@gmail.com>
2020-09-16 16:29:06 +0530

    X00T: Build only required audio hidl hals
    * Drop:
      - android.hardware.audio@2.0-impl
      - android.hardware.audio.effect@2.0-impl
      - android.hardware.audio@5.0
      - android.hardware.audio.common@5.0
      - android.hardware.audio.common@5.0-util
      - android.hardware.audio.effect@5.0
    
    * Build
      - android.hardware.audio@2.0-service
      - android.hardware.audio.effect@2.0-service
    
    Signed-off-by: Pranav Kapoor <pranavkapoor001@gmail.com>

--- hidl-hals.mk
+++ hidl-hals.mk
@@ -16,16 +16,11 @@
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@2.0-service \
-    android.hardware.audio@2.0-impl \
-    android.hardware.audio.effect@2.0-impl \
-    android.hardware.soundtrigger@2.1-impl \
-    android.hardware.audio@5.0 \
-    android.hardware.audio.common@5.0 \
-    android.hardware.audio.common@5.0-util \
     android.hardware.audio@5.0-impl \
-    android.hardware.audio.effect@5.0 \
-    android.hardware.audio.effect@5.0-impl
+    android.hardware.audio@2.0-service \
+    android.hardware.audio.effect@5.0-impl \
+    android.hardware.audio.effect@2.0-service \
+    android.hardware.soundtrigger@2.1-impl
     
 # Bluetooth
 PRODUCT_PACKAGES += \
