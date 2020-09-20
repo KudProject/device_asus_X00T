dfb717710e617b8522e3f02a6763e4c6f13d7498
kubersharma001 <kubersharma001@gmail.com>
2020-09-20 11:36:55 +0530

    X00T: init: SafetyNet madness
     * Switch to walleye oreo build fingerprint and set ro.boot.verifiedbootstate to green
    
    Change-Id: I69fbaa024d117cdd4d6729cb68bacf7a5daacbc3

--- init/init_sdm660.cpp
+++ init/init_sdm660.cpp
@@ -110,4 +110,11 @@ void vendor_load_properties()
     property_override("dalvik.vm.heaptargetutilization", heaptargetutilization);
     property_override("dalvik.vm.heapminfree", heapminfree);
     property_override("dalvik.vm.heapmaxfree", heapmaxfree);
+
+   // Safetynet Workaround
+    property_override("ro.boot.verifiedbootstate", "green");
+    property_override("ro.build.description", "walleye-user 8.1.0 OPM1.171019.011 4448085 release-keys");
+    property_override("ro.build.fingerprint", "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys");
+    property_override("ro.system.build.fingerprint", "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys");
+
 }
