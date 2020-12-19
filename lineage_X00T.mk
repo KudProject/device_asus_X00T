87ac14f36f6399fe679da8324dc6c2769230c3e3
Pranav Kapoor <pranavkapoor001@gmail.com>
2020-12-19 14:27:45 +0530

    X00T: Update build Fingerprint for December SP
    Signed-off-by: Pranav Kapoor <pranavkapoor001@gmail.com>

--- lineage_X00T.mk
+++ lineage_X00T.mk
@@ -34,10 +34,10 @@ PRODUCT_MODEL := ASUS_X00T
 PRODUCT_GMS_CLIENTID_BASE := android-asus
 TARGET_VENDOR := asus
 TARGET_VENDOR_PRODUCT_NAME := X00T
-PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="coral-user 11 RP1A.201105.002 6869500 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="coral-user 11 RQ1A.201205.008 6943376 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := google/coral/coral:11/RP1A.201105.002/6869500:user/release-keys
+BUILD_FINGERPRINT := google/coral/coral:11/RQ1A.201205.008/6943376:user/release-keys
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.build.fingerprint=$(BUILD_FINGERPRINT)
