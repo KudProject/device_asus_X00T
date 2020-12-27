9c622442c6076341331e95db03c51c5eae2b38c8
Pranav Kapoor <pranavkapoor001@gmail.com>
2020-12-27 22:08:05 +0530

    Revert "X00T: Add shim for libdpmframework"
    This reverts commit 4ec39baa59c21bb7e45a7a82191386525ecf434e.

--- device.mk
+++ device.mk
@@ -332,10 +332,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf \
     $(LOCAL_PATH)/configs/sensors/android.hardware.sensors@1.0-service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/android.hardware.sensors@1.0-service.rc
 
-# Shims
-PRODUCT_PACKAGES += \
-    libdpmframework_shim
-
 # Soong
 PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
 
