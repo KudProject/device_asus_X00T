8920629ad542f53e440efb3a66943a0e75cf9c37
Pranav Kapoor <pranavkapoor001@gmail.com>
2021-01-07 11:07:42 +0530

    X00T: Update configs and permissions from LA.UM.9.2.r1-01800-SDMxx0.0
    Signed-off-by: Pranav Kapoor <pranavkapoor001@gmail.com>

--- config.fs
+++ config.fs
@@ -19,6 +19,12 @@ value:2906
 [AID_VENDOR_THERMAL]
 value:2907
 
+[AID_VENDOR_FASTRPC]
+value:2908
+
+[AID_VENDOR_QTR]
+value:2909
+
 [vendor/bin/wcnss_filter]
 mode: 0755
 user: AID_BLUETOOTH
@@ -38,6 +44,18 @@ group: AID_BLUETOOTH
 caps: BLOCK_SUSPEND NET_ADMIN
 
 [system/vendor/bin/hw/android.hardware.bluetooth@1.0-service-qti]
+mode: 0755
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: BLOCK_SUSPEND NET_ADMIN
+
+[vendor/bin/hw/android.hardware.bluetooth@1.0-service-qti-lazy]
+mode: 0755
+user: AID_BLUETOOTH
+group: AID_BLUETOOTH
+caps: BLOCK_SUSPEND NET_ADMIN
+
+[system/vendor/bin/hw/android.hardware.bluetooth@1.0-service-qti-lazy]
 mode: 0755
 user: AID_SYSTEM
 group: AID_SYSTEM
@@ -69,8 +87,8 @@ caps: NET_BIND_SERVICE
 
 [vendor/bin/pd-mapper]
 mode: 0755
-user: AID_RADIO
-group: AID_RADIO
+user: AID_SYSTEM
+group: AID_SYSTEM
 caps: NET_BIND_SERVICE
 
 [system/vendor/bin/pd-mapper]
@@ -116,6 +134,12 @@ group: AID_RADIO
 caps: NET_BIND_SERVICE BLOCK_SUSPEND WAKE_ALARM
 
 [system/vendor/bin/imsrcsd]
+mode: 0755
+user: AID_RADIO
+group: AID_RADIO
+caps: NET_BIND_SERVICE BLOCK_SUSPEND WAKE_ALARM
+
+[vendor/bin/imsdaemon]
 mode: 0755
 user: AID_RADIO
 group: AID_RADIO
