015339cd9777585ddd7a6ed252515fc24c19afd9
Pranav Kapoor <pranavkapoor001@gmail.com>
2021-01-07 11:13:38 +0530

    Revert "X00T: Split privapp permissions for ims/qcrilmsgtunnel move"
    This reverts commit 30e7900b90260111f3a331bc03d3dbc40b389e69.

--- device.mk
+++ device.mk
@@ -291,8 +291,7 @@ PRODUCT_COPY_FILES += \
 
 # QCOM
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/privapp-permissions-qti-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-qti.xml \
-    $(LOCAL_PATH)/configs/privapp-permissions-qti-system.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml
+    $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml
 
 # QMI
 PRODUCT_PACKAGES += \
