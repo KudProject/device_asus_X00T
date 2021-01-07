6c74abf15544a8fa8cc4e7621e49c57195b3140f
Arvind Kumar <karvin@codeaurora.org>
2021-01-07 12:51:50 +0530

    X00T: Enable Health hal 2.1

--- hidl-hals.mk
+++ hidl-hals.mk
@@ -64,8 +64,9 @@ PRODUCT_PACKAGES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.0-impl \
-    android.hardware.health@2.0-service
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-service \
+    android.hardware.health@2.1-impl.recovery
 
 # HIDL
 PRODUCT_PACKAGES += \
