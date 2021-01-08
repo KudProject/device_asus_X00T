96bfb7061040ac25b05ebb0f7f55e4a104219324
Pranav Kapoor <pranavkapoor001@gmail.com>
2021-01-08 11:09:46 +0530

    X00T: Bringup ArrowOS
    Signed-off-by: Pranav Kapoor <pranavkapoor001@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_X00T.mk
+    $(LOCAL_DIR)/arrow_X00T.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_X00T-user \
-    lineage_X00T-userdebug \
-    lineage_X00T-eng
+    arrow_X00T-user \
+    arrow_X00T-userdebug \
+    arrow_X00T-eng
