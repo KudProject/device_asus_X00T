d198630a36733f3be644fb433dab62ecb2058d87
Pranav Kapoor <pranavkapoor001@gmail.com>
2021-01-08 11:09:46 +0530

    X00T: Bringup ArrowOS
    Signed-off-by: Pranav Kapoor <pranavkapoor001@gmail.com>

--- arrow_X00T.mk
+++ arrow_X00T.mk
@@ -19,17 +19,22 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
-# Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Arrow stuff
+$(call inherit-product, vendor/arrow/config/common.mk)
 
 # Inherit from X00T device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
+# Device identifier. This must come after all inclusions.
 PRODUCT_BRAND := asus
 PRODUCT_DEVICE := X00T
 PRODUCT_MANUFACTURER := asus
-PRODUCT_NAME := lineage_X00T
+PRODUCT_NAME := arrow_X00T
 PRODUCT_MODEL := ASUS_X00T
+
+# ArrowOS Properties
+TARGET_BOOT_ANIMATION_RES := 1080
+DEVICE_MAINTAINER := PranavKpr
 
 PRODUCT_GMS_CLIENTID_BASE := android-asus
 TARGET_VENDOR := asus
