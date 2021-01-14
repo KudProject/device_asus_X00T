531ca7060918042f5d79677576f109bc8013142f
EdwinMoq <edwinmmoquete@gmail.com>
2021-01-14 17:45:15 +0530

    X00T: rootdir: Force lz4 compression for ZRAM

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -335,8 +335,6 @@ function configure_zram_parameters() {
     MemTotalStr=`cat /proc/meminfo | grep MemTotal`
     MemTotal=${MemTotalStr:16:8}
 
-    low_ram=`getprop ro.config.low_ram`
-
     # Zram disk - 75% for Go devices.
     # For 512MB Go device, size = 384MB, set same for Non-Go.
     # For 1GB Go device, size = 768MB, set same for Non-Go.
@@ -345,9 +343,7 @@ function configure_zram_parameters() {
     # For >4GB Non-Go device, size = 4GB
     # And enable lz4 zram compression for Go targets.
 
-    if [ "$low_ram" == "true" ]; then
-        echo lz4 > /sys/block/zram0/comp_algorithm
-    fi
+    echo lz4 > /sys/block/zram0/comp_algorithm
 
     if [ -f /sys/block/zram0/disksize ]; then
         if [ -f /sys/block/zram0/use_dedup ]; then
