8b7b39f3509b15f3eb74afc1a5bbae48d301cee8
Pranav Kapoor <pranavkapoor001@gmail.com>
2021-01-22 15:23:57 +0530

    X00T: Update tfa98xx audio patch for R
    Signed-off-by: Pranav Kapoor <pranavkapoor001@gmail.com>

--- audio_patch.sh
+++ audio_patch.sh
@@ -1,3 +1,3 @@
-cp device/asus/X00T/patches/0001-audio-hal-restore-tfa98xx-logic-for-asus-X00T.patch hardware/qcom-caf/msm8998/audio/tfa.patch
+cp device/asus/X00T/patches/0001-X00T-Update-tfa98xx-audio-patch-for-R.patch hardware/qcom-caf/msm8998/audio/tfa.patch
 cd hardware/qcom-caf/msm8998/audio/
 git apply -p1 < tfa.patch > /dev/null 2>&1
