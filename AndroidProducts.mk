26f5fe5c01fd50af7ecd6c97e7d6445aa43d4fa6
Albert I <kras@raphielgang.org>
2021-02-10 23:56:15 +0800

    X00T: dot-ify
    Signed-off-by: Albert I <kras@raphielgang.org>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_X00T.mk
+    $(LOCAL_DIR)/dot_X00T.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_X00T-user \
-    arrow_X00T-userdebug \
-    arrow_X00T-eng
+    dot_X00T-user \
+    dot_X00T-userdebug \
+    dot_X00T-eng
