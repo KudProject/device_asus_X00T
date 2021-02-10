e316cb80d6774e647b7e524852f2f9d6a84faecc
Pranav Kapoor <pranavkapoor001@gmail.com>
2021-02-10 19:10:41 +0530

    X00T: Revert to source built light HAL
    * Reverts:
    - X00T: Rewrite light hal
    - X00T: add permissions for LED and backlight
    - X00T: Import binderized light HAL

--- hidl-hals.mk
+++ hidl-hals.mk
@@ -76,7 +76,8 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service.asus_sdm660
+    android.hardware.light@2.0-impl \
+    android.hardware.light@2.0-service
 
 # LiveDisplay native
 #PRODUCT_PACKAGES += \
