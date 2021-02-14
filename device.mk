6c31b53ba46b27f60eb8562fc5e3d58e566e54e3
Pranav Kapoor <pranavkapoor001@gmail.com>
2021-02-14 22:39:34 +0530

    X00T: Drop audio patch
    * Added tfa commit in 4.19 audio hal
    
    Signed-off-by: Pranav Kapoor <pranavkapoor001@gmail.com>

--- device.mk
+++ device.mk
@@ -69,9 +69,6 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
-# Audio Patch Script
-PATCH := $(shell ($(LOCAL_PATH)/audio_patch.sh))
-
 # Bluetooth
 PRODUCT_PACKAGES += \
     libbthost_if
