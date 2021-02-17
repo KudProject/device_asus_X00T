0b47bdcdf46dffb6fe64f97ae4ef7b2346633eb9
Pranav Kapoor <pranavkapoor001@gmail.com>
2021-02-17 20:47:56 +0530

    X00T: light: Rebrand to asus sdm660
    Signed-off-by: Pranav Kapoor <pranavkapoor001@gmail.com>

--- hidl-hals.mk
+++ hidl-hals.mk
@@ -76,8 +76,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-impl \
-    android.hardware.light@2.0-service
+    android.hardware.light@2.0-service.asus_sdm660
 
 # LiveDisplay native
 #PRODUCT_PACKAGES += \
