7c0b940c6fecff9754c9663d4b3d3c0bfa38d058
Pranav Kapoor <pranavkapoor001@gmail.com>
2021-02-18 22:22:35 +0530

    X00T: Drop audio patch
    Signed-off-by: Pranav Kapoor <pranavkapoor001@gmail.com>

--- audio_patch.sh
+++
@@ -1,3 +0,0 @@
-cp device/asus/X00T/patches/0001-X00T-Update-tfa98xx-audio-patch-for-R.patch hardware/qcom-caf/msm8998/audio/tfa.patch
-cd hardware/qcom-caf/msm8998/audio/
-git apply -p1 < tfa.patch > /dev/null 2>&1
