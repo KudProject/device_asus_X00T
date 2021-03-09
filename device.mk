b84a67c1e43f9327c4e9a2abeb119d0a865881b6
Albert I <kras@raphielgang.org>
2021-03-09 22:06:08 +0800

    X00T: Drop ArrowOS-specific device overlay
    * Smart Charging isn't available on dotOS (yet?)
    
    Signed-off-by: Albert I <kras@raphielgang.org>

--- device.mk
+++ device.mk
@@ -229,8 +229,7 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-arrow
+    $(LOCAL_PATH)/overlay
 
 # Overlays - Enforce RRO
 PRODUCT_ENFORCE_RRO_TARGETS := *
