feeb040577acb12534ab1a8630ecdbd3327da732
SayanAcharya <rowdysayan99@gmail.com>
2021-03-10 11:23:23 +0530

    X00T: Build display config 1.0
    * dependency of liboemcrypto

--- hidl-hals.mk
+++ hidl-hals.mk
@@ -48,6 +48,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
+    vendor.display.config@1.0.vendor \
     vendor.qti.hardware.display.allocator-service
 
 # DRM
