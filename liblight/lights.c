cf39aa5ba0113e3d4eb70ce106439c548f2398f8
Pranav Kapoor <pranavkapoor001@gmail.com>
2021-03-14 18:20:40 +0530

    X00T: liblight: Add dummy button light
    * Silence Light requested not avaliable on this device error
    
    Signed-off-by: Pranav Kapoor <pranavkapoor001@gmail.com>

--- liblight/lights.c
+++ liblight/lights.c
@@ -258,6 +258,16 @@ set_light_attention(struct light_device_t* dev,
     return 0;
 }
 
+static int
+set_light_buttons(struct light_device_t* dev,
+        struct light_state_t const* state)
+{
+    // Dummy function to silence Light not supported error
+    (void)state;
+    (void)dev;
+    return 0;
+}
+
 /** Close the lights device */
 static int
 close_lights(struct light_device_t *dev)
@@ -288,6 +298,10 @@ static int open_lights(const struct hw_module_t* module, char const* name,
         set_light = set_light_battery;
     else if (0 == strcmp(LIGHT_ID_NOTIFICATIONS, name))
         set_light = set_light_notifications;
+    else if (0 == strcmp(LIGHT_ID_BUTTONS, name)) {
+          // set dummy button light
+          set_light = set_light_buttons;
+    }
     else if (0 == strcmp(LIGHT_ID_ATTENTION, name))
         set_light = set_light_attention;
     else
