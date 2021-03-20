b46d55b257523f1e958cc1ead90a2b47a4ef5af9
Mahesh Kumar Sharma <smahesh@codeauroorg.org>
2021-03-20 15:21:03 +0530

    X00T: Update to new ANT HAL interface

--- device.mk
+++ device.mk
@@ -16,11 +16,8 @@
 
 # ANT+
 PRODUCT_PACKAGES += \
-    AntHalService \
+    AntHalService-Soong \
     com.dsi.ant.antradio_library
-
-PRODUCT_COPY_FILES += \
-    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
 
 # Audio
 PRODUCT_PACKAGES += \
