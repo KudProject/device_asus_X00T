d01ee9e9cad069055b73ade1c2bf1e2e5597ddbe
Pranav Kapoor <pranavkapoor001@gmail.com>
2021-03-30 20:53:57 +0530

    X00T: Add gps blobs from LA.UM.9.12.r1-10800-SMxx50.0 and build required packages
    * switch to gps.conf, flp.conf from gps/etc/
    * Add network location overlays
    
    Signed-off-by: Pranav Kapoor <pranavkapoor001@gmail.com>

--- device.mk
+++ device.mk
@@ -113,10 +113,17 @@ PRODUCT_PACKAGES += \
     libqti_vndfwk_detect.vendor
 
 # GPS
+PRODUCT_PACKAGES += \
+    android.hardware.gnss@2.1-impl-qti \
+    android.hardware.gnss@2.1-service-qti \
+    libbatching \
+    libgeofencing \
+    libgnss \
+    libsensorndkbridge \
+    libwifi-hal-ctrl
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps/apdr.conf:$(TARGET_COPY_OUT_VENDOR)/etc/apdr.conf \
-    $(LOCAL_PATH)/configs/gps/flp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/flp.conf \
-    $(LOCAL_PATH)/configs/gps/gps.conf:$(TARGET_COPY_OUT_VENDOR)/etc/gps.conf \
     $(LOCAL_PATH)/configs/gps/izat.conf:$(TARGET_COPY_OUT_VENDOR)/etc/izat.conf \
     $(LOCAL_PATH)/configs/gps/lowi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/lowi.conf \
     $(LOCAL_PATH)/configs/gps/sap.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sap.conf \
