87c9641cde2d4eaaf184d357a8d454fd4a747b37
Saurabh Charde <saurabhchardereal@gmail.com>
2021-04-22 14:32:12 +0530

    X00T: gps: silence warnings
    * Disable these warnings from poping out since they produces to much noice
    
    Signed-off-by: Saurabh Charde <saurabhchardereal@gmail.com>

--- gps/Android.mk
+++ gps/Android.mk
@@ -3,9 +3,9 @@ ifneq ($(BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE),)
 # Set required flags
 GNSS_CFLAGS := \
     -Werror \
-    -Wno-error=unused-parameter \
+    -Wno-unused-parameter \
     -Wno-error=macro-redefined \
-    -Wno-error=reorder \
+    -Wno-reorder \
     -Wno-error=missing-braces \
     -Wno-error=self-assign \
     -Wno-error=enum-conversion \
