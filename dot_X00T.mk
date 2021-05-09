b06b020d10ef43ca513e842606ee43b0038e7782
Albert I <kras@raphielgang.org>
2021-05-10 02:46:07 +0800

    X00T: Switch to Pixel 5 build fingerprint from RQ2A.210505.003
    Signed-off-by: Albert I <kras@raphielgang.org>

--- dot_X00T.mk
+++ dot_X00T.mk
@@ -42,4 +42,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="sdm660_64-user 10 QKQ1 72 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := asus/ASUS_X00TD/ASUS_X00TD:10/QKQ1/17.2017.2012.438-20201203:user/release-keys
+BUILD_FINGERPRINT := google/redfin/redfin:11/RQ2A.210505.003/7255357:user/release-keys
